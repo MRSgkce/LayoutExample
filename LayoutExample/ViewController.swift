72954d7d8fc7a6874d4c8818ff4b4b3b854634a4
MRSgkce <mursidegokce985@gmail.com>
2024-08-09 15:50:15 +0300

    veri aktarımı butona basıldıktan sonra sağlandı.

--- LayoutExample/ViewController.swift
+++ LayoutExample/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var birinciViewController: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    var alinanDeger=" "
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func buton(_ sender: Any) {
+        alinanDeger=textField.text!
+        if alinanDeger=="test"{
+            performSegue(withIdentifier:"toikincivc", sender: nil)
+        }else{
+            birinciViewController.text="tekrar gir"
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier=="toikincivc"{
+            let destinationvc=segue.destination as! ikinciViewController
+            destinationvc.verilenSonuc=alinanDeger
+        }
+    }
 }
 
